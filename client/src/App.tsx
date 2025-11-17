b379efe54546402bfcfbc91b4316e784af581242
mestrereginaldo-site <reginaldo.oliveira@catolicasc.edu.br>
2025-11-17 14:00:08 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
+ import { Switch, Route } from "wouter";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
@@ -50,11 +48,7 @@ function Router() {
 }
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Router />
-    </QueryClientProvider>
-  );
+  return <Router />;
 }
 
 export default App;
