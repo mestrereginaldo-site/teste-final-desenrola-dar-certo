dd954b24d83a42237a7a510044f142ce857ec8c8
mestrereginaldo-site <reginaldo.oliveira@catolicasc.edu.br>
2025-11-17 13:56:29 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  root: ".", // A raiz é onde está o index.html
   build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client/src"),
-    },
-  },
-  publicDir: "public",
+    outDir: "dist"
+  }
 });
